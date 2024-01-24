5005bbd3c60c33614653b0ce5162fb37cdba3add
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-01-24 07:11:31 +0700

    chore: updated content

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ console.log('Run web server on port :', port);
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
+  res.write(`Actually I'am running on port ${port}`); //write a response to the client
   res.end(); //end the response
 }).listen(port); //the server object listens on port 8080 
