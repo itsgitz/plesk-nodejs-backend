fc575557eb9f9dc746e558a9a6a15e88f2b7f15f
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-01-24 07:02:42 +0700

    chore: first commits

---
+++ index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+console.log('Run web server on port :', port);
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(port); //the server object listens on port 8080 
